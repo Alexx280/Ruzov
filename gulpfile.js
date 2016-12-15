381d953d2ad9f83e4e91e1c031b4d4bf5f9fc761
Alex <alexx280@yandex.ru>
2016-12-15 11:54:52 +0700

    Настроено объединение и минимизация подключаемых файлов (js, css).

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require ('gulp'),
     sass = require ('gulp-sass'),
     less = require ('gulp-less'),
+    concat = require ('gulp-concat'),
+    uglify = require ('gulp-uglifyjs'),
+    cssnano = require ('gulp-cssnano'),
+    rename = require ('gulp-rename'),
     browserSync = require ('browser-sync');
 
-gulp.task('sass', function(){
+gulp.task('sass',  function(){
     return gulp.src('app/sass/**/*.sass')
         .pipe(sass())
         .pipe(gulp.dest('app/css'))
@@ -11,11 +15,24 @@ gulp.task('sass', function(){
             stream:true
         }))
 });
+gulp.task('scripts', function(){
+    return gulp.src([
+        'js/jquery.js',
+        'js/vidjet.js',
+        'js/my_code.js'])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('js'))
+});
+gulp.task('css-libs', ['sass', 'less'], function(){
+    return gulp.src('style.css')
+        .pipe(cssnano())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(''))
+});
 gulp.task('less', function(){
-    //return gulp.src('app/less/**/*.less')
     return gulp.src('*.less')
         .pipe(less())
-        //.pipe(gulp.dest('app/css'))
         .pipe(gulp.dest(''))
         .pipe(browserSync.reload({
             stream:true
@@ -31,10 +48,9 @@ gulp.task('browserSync', function(){
     })
 });
 
-gulp.task('watch', ['browserSync', 'sass', 'less'],function(){
+gulp.task('watch', ['browserSync', 'css-libs', 'scripts'],function(){
     gulp.watch('app/sass/**/*.sass', ['sass'])
-    //gulp.watch('app/less/**/*.less', ['less'])
     gulp.watch('*.less', ['less'])
     gulp.watch('*.html', browserSync.reload)
-    gulp.watch('app/js/**/*.js', browserSync.reload)
+    gulp.watch('js/**/*.js', browserSync.reload)
 });
